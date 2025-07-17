1be79f9ddddb3c0b3d95b69f0f024f0f5d41e920
dongkaiqi <87631658@qq.com>
2025-07-17 11:46:54 +0800

    feat: button

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="petal-button"
+    :class="{
+      [`petal-button--${type}`]: type,
+      [`petal-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled || loading"
+  >
+    <PetalIcon icon="spinner" spin v-if="loading" />
+    <PetalIcon :icon="icon" v-if="icon" />
+    <span v-if="$slots.default">
+      <slot></slot>
+    </span>
+  </button>
+</template>
+
+<script setup>
+import propObj from './props'
+defineOptions({
+  name: 'PetalButton',
+})
+defineProps(propObj)
+</script>
+
+<style lang="scss" scoped></style>
